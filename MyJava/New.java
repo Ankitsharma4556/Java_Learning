79488a154e66fb7c0d8f6cc313d205938a5d32c1
Ankit Sharma <iamblackstar5@gmail.com>
2024-09-20 15:49:28 +0530

    2nd Commit

---
+++ MyJava/New.java
@@ -0,0 +1,8 @@
+public class New{
+    public static void main(String[] args) {
+        Main myobj = new Main();
+
+        System.out.println(myobj.x);
+        System.out.println(myobj.y);
+    }
+}
